d552b2ba4cfae5c17a79175406bf0dd56ced387d
Evgeni.ok14 <evgeni.ok@mail.ru>
2022-12-20 18:41:47 +0300

    added a new project: tusk41

---
+++ tusk41/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите элементы массива: ");
+int[] s = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+Console.WriteLine();
+Console.WriteLine($"[{String.Join(", ",s)}]"); 
+int count =0;
+for (int i = 0;  i < s.Length; i++)
+{
+    if (s[i] > 0)
+     {
+        count = count + 1;
+     }
+}
+Console.WriteLine("Количество чисел введённых пользователем, значения которых больше нуля равно:" + " " + count);
